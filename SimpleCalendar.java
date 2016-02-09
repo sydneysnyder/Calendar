d9e168eae85d914b634e54cc0731ee68daf36837
Sydney Snyder <syd.sny@gmail.com>
2016-02-09 13:43:41 -0800

    Create SimpleCalendar.java
    Runs calendar

---
+++ SimpleCalendar.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+/**
+ * Runs the calendar
+ * @author sydneysnyder
+ *
+ */
+public class SimpleCalendar 
+{
+	public static void main (String args[])
+	{
+		CalendarFrame c = new CalendarFrame();		
+	}
+}
